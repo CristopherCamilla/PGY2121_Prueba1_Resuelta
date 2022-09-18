d6b0bab3b18a0bbe81fbc66633a82b523e1e243b
Sebas Contreras <swf.sebas@gmail.com>
2022-09-17 22:48:40 -0300

    termino de validaciones

--- src/main/java/DuocUC/DTO/Usuario.java
+++ src/main/java/DuocUC/DTO/Usuario.java
@@ -1,13 +1,16 @@
 package DuocUC.DTO;
 
+import java.time.Period;
 import java.util.Date;
+import java.time.LocalDate;
 
 public class Usuario {
+    private static int Correlativo = 1000;
     private int ID;
     private String Nombre;
     private int Rut;
     private String Dv;
-    private Date FechaNacimiento;
+    private LocalDate FechaNacimiento;
     private int Telefono;
     private String Email;
     private String NombreUsuario;
@@ -15,21 +18,24 @@ public class Usuario {
 
     public Usuario() {
         this.ID = 0;
-        this.Nombre = "";
+        this.Nombre = "No válido";
         this.Rut = 0;
-        this.Dv = "";
-        this.FechaNacimiento = new Date();
+        this.Dv = "No válido";
+        this.FechaNacimiento = LocalDate.now();
         this.Telefono = 0;
-        this.Email = "";
-        this.NombreUsuario = "";
-        this.Password = "";
+        this.Email = "No válido";
+        this.NombreUsuario = "No válido";
+        this.Password = "No válido";
     }
 
     public int getID() {
         return ID;
     }
-    public void setID(int iD) {
-        ID = iD;
+    public void setID(int ID) {
+        this.ID = ID;
+        if(ID < 0){
+            this.ID = 0;
+        }
     }
     public String getNombre() {
         return Nombre;
@@ -40,43 +46,102 @@ public class Usuario {
     public int getRut() {
         return Rut;
     }
-    public void setRut(int rut) {
-        Rut = rut;
+    public void setRut(int Rut, char Dv) {
+        if(validaRut(Rut, Dv)){
+            this.Rut = Rut;
+            this.Dv = Dv + "";
+        }
     }
+
     public String getDv() {
         return Dv;
     }
-    public void setDv(String dv) {
-        Dv = dv;
-    }
-    public void setFechaNacimiento(Date fechaNacimiento) {
-        FechaNacimiento = fechaNacimiento;
-    }
-    public Date getFechaNacimiento() {
+
+    public LocalDate getFechaNacimiento() {
         return FechaNacimiento;
+    }
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        Period periodo = Period.between(LocalDate.now(), fechaNacimiento);
+        if(periodo.getYears() > 18){
+            FechaNacimiento = fechaNacimiento;
+        }
     }
     public int getTelefono() {
         return Telefono;
     }
     public void setTelefono(int telefono) {
-        Telefono = telefono;
+        String strTelefono = telefono +"";
+        if(strTelefono.length() > 6 && strTelefono.startsWith("56")){
+            Telefono = telefono;
+        }
     }
     public String getEmail() {
         return Email;
     }
     public void setEmail(String email) {
-        Email = email;
+        if(email.contains("@") ||email.contains(".com") || email.contains(".")){
+            Email = email;
+        }
     }
     public String getNombreUsuario() {
         return NombreUsuario;
     }
     public void setNombreUsuario(String nombreUsuario) {
-        NombreUsuario = nombreUsuario;
+        if(nombreUsuario.length() >= 4){
+            NombreUsuario = nombreUsuario;
+        }
     }
     public String getPassword() {
         return Password;
     }
     public void setPassword(String password) {
-        Password = password;
+        if(validaPassword(password)){
+            Password = password;
+        }
+    }
+
+    private boolean validaPassword(String password){
+        boolean resultSimbolos = false;
+        boolean resultMayuscula = false;
+        boolean resultNumeros = false;
+
+        if(password.length() < 6){
+            return false;
+        }
+
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+            if(Character.isUpperCase(c)){
+                resultMayuscula = true;
+            }
+            else if(Character.isDigit(c)){
+                resultNumeros = true;
+            }
+            else if(Character.isLowerCase(c)){
+                // hacer nada
+            }
+            else{
+                resultSimbolos = true;
+            }
+        }
+
+        return resultSimbolos && resultMayuscula && resultNumeros;
+    }
+
+    public boolean validaRut(int rut, char dv) {
+        boolean validacion = false;
+        try {
+            int m = 0, s = 1;
+            for (; rut != 0; rut /= 10) {
+                s = (s + rut % 10 * (9 - m++ % 6)) % 11;
+            }
+            if (dv == (char) (s != 0 ? s + 47 : 75)) {
+                validacion = true;
+            }
+
+        } catch (java.lang.NumberFormatException e) {
+        } catch (Exception e) {
+        }
+        return validacion;
     }
 }
--- src/main/java/DuocUC/Prueba1/App.java
+++ src/main/java/DuocUC/Prueba1/App.java
@@ -1,8 +1,10 @@
 package DuocUC.Prueba1;
 
 import java.util.Locale;
+import java.util.regex.Pattern;
 
 import DuocUC.DTO.ClubDeportivo;
+import DuocUC.DTO.Usuario;
 
 /**
  * Hello world!
@@ -12,23 +14,31 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("¡Bienvenido! Estamos configurando la aplicación para ti...");
         Locale chileLocale = new Locale("es", "CL");
         Locale.setDefault(chileLocale);
+        
+        System.out.println("===========================");
+        System.out.println("Creando Usuarios:");
+        Usuario user1 = new Usuario();
+        
 
+        System.out.println("===========================");
+        System.out.println("Creando Clubes Deportivos:");
         ClubDeportivo cd_ar = new ClubDeportivo();
         cd_ar.setNombre("Arsenal de Coquimbo");
         cd_ar.setLema("Cañones a los puertos");
         cd_ar.setValorSuscripcion(10050);
         cd_ar.setColores("Amarillo, Rojo");
+        System.out.println("Creado Club Deportivo "+ cd_ar.getNombre() + " ;; detalle: "+ cd_ar);
 
         ClubDeportivo cd_mc = new ClubDeportivo();
         cd_mc.setNombre("Manquehue City");
         cd_mc.setLema("Vivir y Fuerza");
         cd_mc.setValorSuscripcion(15100);
         cd_mc.setColores("Celeste, Blanco");
+        System.out.println("Creado Club Deportivo "+ cd_mc.getNombre() + " ;; detalle: "+ cd_mc);
 
-        System.out.println(cd_ar);
-        System.out.println(cd_mc);
+        System.out.println("===========================");
     }
 }
